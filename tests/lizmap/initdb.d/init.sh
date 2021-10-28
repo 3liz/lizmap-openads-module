6e01d9e94dcf6437888d775cafc7c41419ccc1a6
Nicolas Boisteault <nboisteault@3liz.com>
2021-10-28 16:26:17 +0200

    Create pg schema and QGIS layers for test project

--- tests/lizmap/initdb.d/init.sh
+++ tests/lizmap/initdb.d/init.sh
@@ -8,3 +8,7 @@ EOSQL
 psql --username postgres --no-password -d lizmap <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;
 EOSQL
+
+# Let .sql.ext extension to avoid postgresql docker to run *.sql file
+# Ref: look for docker-entrypoint-initdb.d in https://hub.docker.com/_/postgres/
+psql --username postgres --no-password -d lizmap -f /docker-entrypoint-initdb.d/schema_openads.sql.ext

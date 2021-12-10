463a125f155f19ee2ea5d2c3a1155a9aa9875c4b
Nicolas Boisteault <nboisteault@3liz.com>
2021-12-10 14:38:42 +0100

    Test existence getCustomProjectVariables() Fix #20

--- openads/classes/openads.listener.php
+++ openads/classes/openads.listener.php
@@ -18,23 +18,32 @@ class openadsListener extends jEventListener
         $project = $event->project;
         $p = lizmap::getProject($repository.'~'.$project);
         if (!$p || $project !== 'openads') {
+            \jLog::log('Le module openads nécessite que le projet se nomme "openads"', 'error');
+
             return;
         }
 
-        $js = [jUrl::get('jelix~www:getfile', array('targetmodule' => 'openads', 'file' => 'openads.js'))];
+        $jscode = array();
 
-        $customProjectVariables = $p->getCustomProjectVariables();
-
-        if($customProjectVariables && $customProjectVariables['openads_url_ads']){
-            $jscode = array('const openads_url_ads = "'.$customProjectVariables['openads_url_ads'].'";');
+        if (method_exists($p, 'getCustomProjectVariables')) {
+            $customProjectVariables = $p->getCustomProjectVariables();
+            if ($customProjectVariables && $customProjectVariables['openads_url_ads']) {
+                $jscode = array('const openads_url_ads = "'.$customProjectVariables['openads_url_ads'].'";');
+            } else {
+                $jscode = array('console.warn(`La variable "openads_url_ads" doit être définie dans votre projet QGIS.`);');
+            }
         }else{
-            $jscode = array('console.warn(`La variable "openads_url_ads" doit être définie dans votre projet QGIS.`);');
+            \jLog::log('Le module openads nécessite Lizmap 3.5.0 ou 3.4.8 minimum', 'error');
+
+            return;
         }
+
+        $js = array(jUrl::get('jelix~www:getfile', array('targetmodule' => 'openads', 'file' => 'openads.js')));
 
         $event->add(
             array(
                 'js' => $js,
-                'jscode' => $jscode
+                'jscode' => $jscode,
             )
         );
     }

58f1229626bd09ac5e49413ff903cde3809ad9b6
Nicolas Boisteault <nboisteault@3liz.com>
2021-12-15 11:36:58 +0100

    Seul le paramètre dossier => zoom sur l'objet et sélection

--- openads/www/openads.js
+++ openads/www/openads.js
@@ -25,7 +25,7 @@ lizMap.events.on({
         const urlSearchParams = new URLSearchParams(window.location.search);
         const params = Object.fromEntries(urlSearchParams.entries());
 
-        if (params && params.parcelles){
+        if (params && params.parcelles) {
 
             // Construction de la requête de récupération de
             // l'emprise pour la/les parcelles en paramètre
@@ -37,7 +37,7 @@ lizMap.events.on({
             sentFormData.append('TYPENAME', NOM_COUCHE_PARCELLES);
             sentFormData.append('OUTPUTFORMAT', 'GeoJSON');
             sentFormData.append('GEOMETRYNAME', 'extent');
-            
+
             // Transformation du paramètre d'URL parcelles en EXP_FILTER
             // e.g. : 800016000AK0145;800016000ZA0002 => '800016000AK0145','800016000ZA0002'
             const parcelleIdentForExp_Filter = params.parcelles.split(';').map((parcelleIdent) => `'${parcelleIdent}'`).join(',');
@@ -57,7 +57,7 @@ lizMap.events.on({
                         // L'emprise zoomée est celle de l'ensemble des parcelles
                         extent = extend(extent, feature.bbox);
                         // ids des parcelles
-                        parcellesIds.push(feature.id.split(NOM_COUCHE_PARCELLES+'.')[1]);
+                        parcellesIds.push(feature.id.split(NOM_COUCHE_PARCELLES + '.')[1]);
                     }
 
                     // Conversion de l'extent de 4326 vers la projection de la carte
@@ -101,7 +101,8 @@ lizMap.events.on({
                 return response.json();
             }).then(function (data) {
                 if (data?.features.length > 0) {
-                    let extent = data.features[0].bbox;
+                    const extent = data.features[0].bbox;
+                    const fid = data.features[0].id.split(NOM_COUCHE_DOSSIER + '.')[1];
 
                     // Conversion de l'extent de 4326 vers la projection de la carte
                     // TODO : expose OL6 transformExtent in Lizmap
@@ -110,6 +111,13 @@ lizMap.events.on({
                     const bottomright = lizMap.mainLizmap.transform([extent[2], extent[3]], 'EPSG:4326', lizMap.mainLizmap.projection);
                     // Zoom sur l'emprise du dossier en paramètre
                     lizMap.mainLizmap.map.getView().fit([topleft[0], topleft[1], bottomright[0], bottomright[1]]);
+
+                    // Sélection s'il n'y a pas de paramètre `parcelles` dans l'URL
+                    if (!params.parcelles) {
+                        lizMap.events.triggerEvent("layerfeatureselected",
+                            { 'featureType': NOM_COUCHE_DOSSIER, 'fid': fid, 'updateDrawing': true }
+                        );
+                    }
                 }
             });
         }
